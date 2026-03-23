a0bf02eb0f7bcc1d16d92bf3e4869e2145b2ce1c
Nuudal <hoang.thb.65cntt@ntu.edu.vn>
2026-03-23 16:44:07 +0700

    Bai TH7 part 2

--- BaiTH7_ListView1/app/src/main/java/till/edu/baith7_listview1/MainActivity.java
+++ BaiTH7_ListView1/app/src/main/java/till/edu/baith7_listview1/MainActivity.java
@@ -1,8 +1,11 @@
 package till.edu.baith7_listview1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +17,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<String> danhSachDuLieu = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,12 +32,13 @@ public class MainActivity extends AppCompatActivity {
 
         ListView lvDanhSach = findViewById(R.id.lv_danh_sach);
 
-        ArrayList<String> danhSachDuLieu = new ArrayList<>();
-        danhSachDuLieu.add("Android");
-        danhSachDuLieu.add("Java");
-        danhSachDuLieu.add("PHP");
-        danhSachDuLieu.add("Python");
-        danhSachDuLieu.add("C#");
+
+        danhSachDuLieu.add("Hà Nội");
+        danhSachDuLieu.add("Hồ Chí Minh");
+        danhSachDuLieu.add("Đồng Nai");
+        danhSachDuLieu.add("Bình thuận");
+        danhSachDuLieu.add("Ninh Thuận");
+        danhSachDuLieu.add("Nha Trang");
 
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
@@ -42,7 +47,19 @@ public class MainActivity extends AppCompatActivity {
 
         lvDanhSach.setAdapter(adapter);
 
+        lvDanhSach.setOnItemClickListener(BoLangNghevaXL);
+
+
     }
+
+    AdapterView.OnItemClickListener BoLangNghevaXL = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+            String strdanhsach = danhSachDuLieu.get(position);
+
+            Toast.makeText(MainActivity.this, strdanhsach, Toast.LENGTH_LONG).show();
+        }
+    };
 
 
 }
