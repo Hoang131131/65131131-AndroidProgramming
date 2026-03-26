5bcd4c5709417c3c114ffa6357957f6a2193b917
Nuudal <hoang.thb.65cntt@ntu.edu.vn>
2026-03-26 07:47:49 +0700

    Update build.gradle.kts

--- WeatherForecast/app/build.gradle.kts
+++ WeatherForecast/app/build.gradle.kts
@@ -43,6 +43,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("com.github.bumptech.glide:glide:4.15.1")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
+    implementation("com.github.bumptech.glide:glide:5.0.5")
+    annotationProcessor("com.github.bumptech.glide:compiler:5.0.5")
 }
