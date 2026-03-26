aa9261849c8cd1dc87745499be7bbe2f7f43e168
Nuudal <hoang.thb.65cntt@ntu.edu.vn>
2026-03-26 20:17:23 +0700

    Update RSSParser.java

--- NewsApp/app/src/main/java/ntu/hoang/newsapp/RSSParser.java
+++ NewsApp/app/src/main/java/ntu/hoang/newsapp/RSSParser.java
@@ -12,32 +12,26 @@ public class RSSParser {
 
     public static List<News> getRSS(String urlString) {
         List<News> list = new ArrayList<>();
-
         try {
             URL url = new URL(urlString);
             InputStream input = url.openStream();
-
             XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
             XmlPullParser parser = factory.newPullParser();
             parser.setInput(input, null);
 
             boolean insideItem = false;
-
-            String title="", link="", pubDate="", desc="", imgUrl="";
+            String title = "", link = "", pubDate = "", desc = "", imgUrl = "", summary = "";
 
             int eventType = parser.getEventType();
-
             while (eventType != XmlPullParser.END_DOCUMENT) {
                 String tagName = parser.getName();
 
                 switch (eventType) {
-
                     case XmlPullParser.START_TAG:
                         if (tagName.equalsIgnoreCase("item")) {
                             insideItem = true;
-                        }
-
-                        if (insideItem) {
+                            title = link = pubDate = desc = imgUrl = summary = "";
+                        } else if (insideItem) {
                             if (tagName.equalsIgnoreCase("title")) {
                                 title = parser.nextText();
                             } else if (tagName.equalsIgnoreCase("link")) {
@@ -45,39 +39,38 @@ public class RSSParser {
                             } else if (tagName.equalsIgnoreCase("pubDate")) {
                                 pubDate = parser.nextText();
                             } else if (tagName.equalsIgnoreCase("description")) {
-                                desc = parser.nextText();
+                                desc = parser.nextText(); // Lấy toàn bộ nội dung trong description
 
-                                // lấy ảnh
+                                // Xử lý lấy imgUrl từ chuỗi desc
                                 if (desc.contains("src=\"")) {
-                                    int start = desc.indexOf("src=\"") + 5;
-                                    int end = desc.indexOf("\"", start);
-                                    imgUrl = desc.substring(start, end);
-
-                                    int q = imgUrl.indexOf("?");
-                                    if (q > 0) imgUrl = imgUrl.substring(0, q);
+                                    try {
+                                        int start = desc.indexOf("src=\"") + 5;
+                                        int end = desc.indexOf("\"", start);
+                                        imgUrl = desc.substring(start, end);
+                                    } catch (Exception e) {
+                                        imgUrl = ""; // Tránh crash nếu description trống
+                                    }
                                 }
 
-                                String summary = desc.substring(desc.lastIndexOf("</br>") + 5).trim();
-
-                                list.add(new News(title, link, pubDate, imgUrl, summary));
+                                // Lấy summary bằng cách xóa hết các thẻ HTML
+                                summary = desc.replaceAll("<[^>]*>", "").trim();
                             }
                         }
                         break;
 
                     case XmlPullParser.END_TAG:
                         if (tagName.equalsIgnoreCase("item")) {
+                            // CHỈ ADD VÀO LIST KHI KẾT THÚC THẺ ITEM
+                            list.add(new News(title, link, pubDate, imgUrl, summary));
                             insideItem = false;
                         }
                         break;
                 }
-
                 eventType = parser.next();
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return list;
     }
 }
