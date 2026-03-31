ed0cbe6c4dc48cd85b83d430ce95c23d7895fc62
Nuudal <hoang.thb.65cntt@ntu.edu.vn>
2026-03-31 10:39:10 +0700

    BaiTH13.ThayDoiFragment

---
+++ BaiTH13ThayDoiFragment/app/src/main/java/ntu/hoang/baith13thaydoifragment/MainActivity.java
@@ -0,0 +1,21 @@
+package ntu.hoang.baith13thaydoifragment;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+    }
+}
