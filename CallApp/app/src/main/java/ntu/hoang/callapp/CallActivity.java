851efacdbd12ed049f6f250b74f2ef974b762f65
Nuudal <hoang.thb.65cntt@ntu.edu.vn>
2026-03-31 14:18:24 +0700

    Bài làm thêm ứng dụng gọi điện

---
+++ CallApp/app/src/main/java/ntu/hoang/callapp/CallActivity.java
@@ -0,0 +1,28 @@
+package ntu.hoang.callapp;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+public class CallActivity extends AppCompatActivity {
+
+    TextView tvPhoneNumber;
+    MaterialButton btnEndCall;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_called);
+
+        tvPhoneNumber = findViewById(R.id.tvPhoneNumber);
+        btnEndCall = findViewById(R.id.btnEndCall);
+
+        String phone = getIntent().getStringExtra("phone");
+        tvPhoneNumber.setText(phone);
+
+        btnEndCall.setOnClickListener(v -> finish());
+    }
+}
