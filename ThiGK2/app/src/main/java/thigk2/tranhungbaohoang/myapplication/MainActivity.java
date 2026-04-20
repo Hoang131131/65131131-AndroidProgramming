c69cf6a31091bcb192f86bdbcb0ee609acf49a15
Nuudal <hoang.thb.65cntt@ntu.edu.vn>
2026-04-20 07:40:59 +0700

    LẦN THỨ 1

---
+++ ThiGK2/app/src/main/java/thigk2/tranhungbaohoang/myapplication/MainActivity.java
@@ -0,0 +1,42 @@
+package thigk2.tranhungbaohoang.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame,new RectangleFragment()).commit();
+        BottomNavigationView btmnav = findViewById(R.id.btmnavview);
+
+        btmnav.setOnItemSelectedListener(item->{
+            Fragment fragment = null;
+            int id = item.getItemId();
+            if (id==R.id.btnfunc1)
+                fragment=new RectangleFragment();
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame, fragment).commit();
+            return true;
+        });
+
+    }
+}
